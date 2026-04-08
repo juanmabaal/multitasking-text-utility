d723492ca7837c9a3fa768b650639d7d0e3becf1
Juan Manuel Balaguera <juanma.baal@gmail.com>
2026-04-08 00:10:46 -0500

    feat: improve feedback flow and add conditional refinement logic

--- src/feedback.py
+++ src/feedback.py
@@ -8,6 +8,7 @@ def feedback_response(user_question: str, first_response: dict[str, Any] ) -> di
     system_prompt = (
         "Eres un auditor critico de calidad de respuestas de atencion al cliente y prompting"
         "Evaluas categoria, prioridad, respuesta, acciones y status"
+        "Debes devolver únicamente JSON válido."
     )
 
     user_prompt = f"""
@@ -19,49 +20,51 @@ def feedback_response(user_question: str, first_response: dict[str, Any] ) -> di
 
         Evalúa la calidad de la respuesta asignando un puntaje de 0.0 a 1.0 para cada criterio:
 
-        Evalua la respuesta inicial con esta calificacion (0 - 1 por criterio)
             category
-                Clasificación: ¿Corresponde al departamento o área temática correcta del negocio?
+            Clasificación: ¿Corresponde al departamento o área temática correcta del negocio?
 
             priority
-                Urgencia: ¿Se alinea con el nivel de impacto o riesgo para el usuario?
+            Urgencia: ¿Se alinea con el nivel de impacto o riesgo para el usuario?
 
             answer
-                Resolución: ¿La explicación aborda directamente la raíz del problema?
-                Tono: ¿Mantiene la voz de marca y el nivel de empatía esperado?
+            Resolución: ¿La explicación aborda directamente la raíz del problema?
+            Tono: ¿Mantiene la voz de marca y el nivel de empatía esperado?
 
             actions
-                Eficacia: ¿Los pasos sugeridos solucionan el problema o son redundantes?
-                Viabilidad: ¿El usuario tiene los permisos y herramientas para ejecutar lo indicado?
+            Eficacia: ¿Los pasos sugeridos solucionan el problema o son redundantes?
+            Viabilidad: ¿El usuario tiene los permisos y herramientas para ejecutar lo indicado?
 
             status
-                Protocolo: ¿El flujo de trabajo (humano o automático) es el adecuado para este caso?
+            Protocolo: ¿El flujo de trabajo (humano o automático) es el adecuado para este caso?
 
-        Si encuentras problemas relevantes, genera una versión mejorada de la respuesta.
+            Reglas:
+            - Si algún score es menor a 0.8, entonces "should_refine" debe ser true.
+            - Si todos los scores son mayores o iguales a 0.8, entonces "should_refine" debe ser false.
+            - "issues_detected" debe contener una lista breve y clara de problemas encontrados.
+            - Si no hay problemas importantes, devuelve una lista vacía en "issues_detected".
 
-        Devuelve únicamente JSON válido con la siguiente estructura EXACTA:
+            Devuelve únicamente JSON válido con la siguiente estructura EXACTA:
 
-        {{
+            {{
             "feedback_output": {{
                 "scores": {{
-                    "category": 0.0,
-                    "priority": 0.0,
-                    "answer": 0.0,
-                    "actions": 0.0,
-                    "status": 0.0
+                "category": 0.0,
+                "priority": 0.0,
+                "answer": 0.0,
+                "actions": 0.0,
+                "status": 0.0
                 }},
-                "category": "billing",
-                "priority": "medium",
-                "answer": "Texto mejorado de respuesta",
-                "actions": ["Acción 1", "Acción 2", "Acción 3", "Acción 4"],
-                "status": "needs_human_review"
+                "should_refine": true,
+                "issues_detected": [
+                "Describe aquí un problema detectado"
+                ]
             }}
-        }}
-    """.strip()
+            }}
+            """.strip()
 
     feedback_result = call_json_and_build_metrics(system_prompt, user_prompt)
 
     return {
-        "final_response": feedback_result['response'],
+        "feedback_response": feedback_result['response'],
         "feedback_metrics": feedback_result['metrics']
     }
--- src/run_query.py
+++ src/run_query.py
@@ -2,24 +2,55 @@
 import json
 from llm_client import get_initial_support_response
 from feedback import feedback_response
+from refiner import refine_response
 
 def main():
     print('Hola!, Estamos para servirle. Diganos como le podemos ayudar')
     user_request = input('Tu: ')
+
     initial_result = get_initial_support_response(user_request)
+    reviewed = feedback_response(user_request, initial_result)
     
-    print("=" * 80)
-    print(f"Respuesta Inicial de : {user_request}")
+    total_tokens = initial_result['initial_metrics']['total_tokens']  + reviewed['feedback_metrics']['total_tokens']
+
+    total_usd = initial_result['initial_metrics']['estimated_cost_usd']  + reviewed['feedback_metrics']['estimated_cost_usd']
+
+    total_latency = initial_result['initial_metrics']['latency_ms']  + reviewed['feedback_metrics']['latency_ms']
+
+    should_refine = reviewed['feedback_response']['feedback_output']['should_refine']
+
     print("\n" + "=" * 80)
-    print("\n=== Respuesta Inicial===")
+    print("=== Respuesta Inicial ===")
     print(json.dumps(initial_result, ensure_ascii=False, indent=2))
 
     print("\n" + "=" * 80)
-    reviewed = feedback_response(user_request, initial_result)
-    print("\n=== Respuesta Feedback ===")
-    print("\n" + "=" * 80)
+    print("=== Feedback ===")
     print(json.dumps(reviewed, ensure_ascii=False, indent=2))
 
+    if should_refine:
+          refined = refine_response(user_request, initial_result, reviewed)
+          final_response = refined['refined_response']
+          print("\n" + "=" * 80)
+          print("=== Refined ===")
+          print(json.dumps(refined, ensure_ascii=False, indent=2))
+
+          total_tokens += refined['refiner_metrics']['total_tokens']
+          total_usd += refined['refiner_metrics']['estimated_cost_usd']
+          total_latency += refined['refiner_metrics']['latency_ms']
+    
+    else: 
+         final_response = initial_result['Initial_response']
+    
+
+    print("\n" + "=" * 80)
+    print("=== Final Response ===")
+    print(json.dumps(final_response, ensure_ascii=False, indent=2))
+
+    print("\n" + "=" * 80)
+    print("=== metricas ===")
+    print(f"Total de Tokens consumidos: {total_tokens}")
+    print(f"Gastos en USD estimado: {total_usd}")
+    print(f"El tiempo total de espera para obtener la respuesta fue de: {total_latency} milisegundos")
 
 if __name__ == '__main__':
     main()
